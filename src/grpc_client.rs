73b39dd64e249cf5f968b2f929dcf64fc9690ebf
riorio805 <sefrianojieftara@gmail.com>
2024-04-23 17:38:24 +0700

    uncomment code

--- src/grpc_client.rs
+++ src/grpc_client.rs
@@ -16,27 +16,27 @@ use services::{
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let mut client =
-    //     PaymentServiceClient::connect("http://[::1]:50051").await?;
-    //
-    // let request = tonic::Request::new(PaymentRequest {
-    //     user_id: "user_123".to_string(),
-    //     amount: 100.0
-    // });
-    // let response = client.process_payment(request).await?;
-    // println!("Response = {:?}", response);
-    //
-    // let mut transaction_client =
-    //     TransactionServiceClient::connect("http://[::1]:50051").await?;
-    // let request = tonic::Request::new(TransactionRequest {
-    //     user_id: "user_123".to_string(),
-    // });
-    //
-    // let mut stream =
-    //     transaction_client.get_transaction_history(request).await?.into_inner();
-    // while let Some(transaction) = stream.message().await? {
-    //     println!("Transaction: {:?}", transaction);
-    // }
+    let mut client =
+        PaymentServiceClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(PaymentRequest {
+        user_id: "user_123".to_string(),
+        amount: 100.0
+    });
+    let response = client.process_payment(request).await?;
+    println!("Response = {:?}", response);
+
+    let mut transaction_client =
+        TransactionServiceClient::connect("http://[::1]:50051").await?;
+    let request = tonic::Request::new(TransactionRequest {
+        user_id: "user_123".to_string(),
+    });
+
+    let mut stream =
+        transaction_client.get_transaction_history(request).await?.into_inner();
+    while let Some(transaction) = stream.message().await? {
+        println!("Transaction: {:?}", transaction);
+    }
 
     let channel = Channel::from_static("http://[::1]:50051").connect().await?;
     let mut client = ChatServiceClient::new(channel);
